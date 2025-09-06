c46370235e9ef6d5d604c904fe36b6f3db3841b7
McShinyShoe <mcshinyshoe@protonmail.com>
2025-09-06 22:07:28 +0700

    feat: Moved authentication to their own controller

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TownController;
-use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 
@@ -14,17 +14,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Authentication Routes
-Route::post('/login', function (Request $request) {
-    $user = User::where('mc_name', $request->mc_name)->first();
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        return make_response(null, 'Invalid credentials', 401, false);
-    }
-    return make_response(['token' => $user->createToken('api-token')->plainTextToken], 'Login successful');
-});
-Route::post('/logout', function (Request $request) {
-    $request->user()->currentAccessToken()->delete();
-    return make_response(null, 'Token revoked');
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // Town Routes
 Route::middleware('auth:sanctum')->group(function () {
